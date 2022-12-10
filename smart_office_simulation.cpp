f2ecefdab8e7fdd90ce77855c398de30ef64d705
Dhairyabhatt99 <71652817+Dhairyabhatt99@users.noreply.github.com>
2022-12-09 23:46:46 -0500

    Update smart_office_simulation.cpp

---
+++ smart_office_simulation.cpp
@@ -0,0 +1,36 @@
+int value;
+int redled = 2;
+int greenled = 3;
+int buzzer = 4;
+int sensor = A0;
+int sensorThresh = 400;
+
+void setup()
+{
+pinMode(redled, OUTPUT);
+pinMode(greenled,OUTPUT);
+pinMode(buzzer,OUTPUT);
+pinMode(sensor,INPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+  int analogValue = analogRead(sensor);
+  Serial.print(analogValue);
+  
+  if(analogValue>sensorThresh)
+  {
+    digitalWrite(redled,HIGH);
+    digitalWrite(greenled,LOW);
+    tone(buzzer,1000,10000);
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(greenled,HIGH);
+    digitalWrite(redled,LOW);
+    noTone(buzzer);
+    delay(1000);
+  }    
+}
